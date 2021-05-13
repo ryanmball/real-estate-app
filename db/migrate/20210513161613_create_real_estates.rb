52b562e81005e0200a2c79b941f074ea078b2958
ryan <ball.ryan.rb@gmail.com>
2021-05-13 10:18:43 -0600

    initial commit

---
+++ db/migrate/20210513161613_create_real_estates.rb
@@ -0,0 +1,17 @@
+class CreateRealEstates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :real_estates do |t|
+      t.text :description
+      t.integer :year_built
+      t.integer :square_feet
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.integer :floors
+      t.string :availability
+      t.string :address
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
